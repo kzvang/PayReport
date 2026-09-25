984d57d59e2a492306bf8f3bdd91350f2c17a809
Vang, Ka Zoua <200164035@fvtc.edu>
2026-09-24 19:42:36 -0500

    Add project files.

---
+++ KZV.PayReport/Main.cpp
@@ -0,0 +1,85 @@
+
+
+//Pay Report
+//Assignment 4
+// Ka Zoua Vang
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+struct Employee {
+    int ID;
+    string FirstName;
+    string LastName;
+    float HoursWorked;
+    float HourlyRate;
+};
+
+Employee* GetEmployeeInfo(int i)
+{
+    Employee* pE = new Employee;
+    cout << "Enter the ID for Employee "<< i + 1 << ": ";
+    cin >> pE->ID;
+    cout << "Enter the First Name for Employee: ";
+    cin >> pE->FirstName;
+    cout << "Enter the Last Name for Employee: ";
+    cin >> pE->LastName;
+    cout << "Enter the Hours Worked for Employee: ";
+    cin >> pE->HoursWorked;
+    cout << "Enter the Hourly Rate for Employee: ";
+    cin >> pE->HourlyRate;
+    return pE;
+}
+
+void PrintWeeklyPay(Employee* pEmployees, int size)
+{
+    cout << "Pay Report \n";
+    cout << "------------ \n";
+
+    for (int i = 0; i < size; i++) {
+
+        cout << pEmployees[i].ID << ". " << pEmployees[i].FirstName << " " 
+            << pEmployees[i].LastName << ": $" << pEmployees[i].HoursWorked * pEmployees[i].HourlyRate << "\n";
+
+    }
+
+    
+}
+
+void PrintTotalPay(Employee* pEmployees, int size)
+{
+    int Total = 0;
+
+    for (int i = 0; i < size; i++) {
+
+        Total += (pEmployees[i].HourlyRate * pEmployees[i].HoursWorked);
+    }
+
+    cout << "\nTotal Pay: $" << Total;
+}
+
+int main()
+{
+    cout << "Enter the number of employees: ";
+    int size = 0;
+    cin >> size;
+    cout << "\n";
+
+    Employee* employees = new Employee[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        employees[i] = *GetEmployeeInfo(i);
+        cout << "\n";
+    }
+
+    PrintWeeklyPay(employees, size);
+    PrintTotalPay(employees, size);
+
+    delete[] employees;
+
+    (void)_getch();
+    return 0;
+}
